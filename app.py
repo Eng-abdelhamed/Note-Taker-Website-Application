e7911bf9f55d6c82d59b6203633fabb88e4923b7
abdelhamed ahmed <eng.abdelhamedahmed10@gmail.com>
2026-02-05 00:41:02 +0000

    Project File Added

---
+++ app.py
@@ -0,0 +1,35 @@
+
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST']     ='localhost'
+app.config['MYSQL_USER']     ='notes_user'
+app.config['MYSQL_PASSWORD'] ='rra31CB$'
+app.config['MYSQL_DB']       ='notesdb'
+
+mysql=MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        note = request.form['note']
+        cur = mysql.connection.cursor()
+        cur.execute(
+            "INSERT INTO notes (content) VALUES (%s)", (note,)
+        )
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/')
+
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "SELECT content, created_at FROM notes ORDER BY created_at DESC"
+    )
+    notes = cur.fetchall()
+    cur.close()
+
+    return render_template('index.html', notes=notes)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
